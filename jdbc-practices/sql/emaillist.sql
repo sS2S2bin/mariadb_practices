394ac2bf946aa0c59b83969e254ed95ce76c529c
sS2S2bin <3dltnqls@naver.com>
2024-05-24 16:56:26 +0900

    webdb model update

--- jdbc-practices/sql/emaillist.sql
+++ jdbc-practices/sql/emaillist.sql
@@ -18,5 +18,14 @@ select * from author;
 
 select * from author;
 select * from book a, author b where a.author_no and b.no order by a.no desc;
-select * from book;
+select * from book; 
 select a.no, a.title, a.status, b.name from book a, author b where a.author_no and b.no order by b.no desc;
+
+select * from emaillist;
+delete from emaillist where no=12;
+select * from guestbook;
+select no from guestbook;
+select last_insert_id() from guestbook;
+delete from emaillist;
+
+insert into guestbook(6, 3,3,3,now());
